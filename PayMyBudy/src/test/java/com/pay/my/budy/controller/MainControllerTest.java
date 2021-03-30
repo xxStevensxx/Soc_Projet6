efbe83b2685105d08753e9052dc57065706436e7
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-03-30 23:46:32 +0200

    Creation des Controllers en TDD

---
+++ PayMyBudy/src/test/java/com/pay/my/budy/controller/MainControllerTest.java
@@ -0,0 +1,53 @@
+package com.pay.my.budy.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class MainControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+
+	@Test
+	public void homeControllerTest() throws Exception {
+		
+		//WHEN
+		
+		
+		//GIVEN
+		
+		
+		//THEN
+		mockMvc.perform(get("/home")).andExpect(status().isOk());
+				
+		
+		
+	}
+	
+	
+	@Test
+	public void loginControllerTest() throws Exception {
+		
+		//WHEN
+		
+		
+		//GIVEN
+		
+		
+		//THEN
+		mockMvc.perform(get("/login")).andExpect(status().isOk());
+				
+		
+		
+	}
+
+}
