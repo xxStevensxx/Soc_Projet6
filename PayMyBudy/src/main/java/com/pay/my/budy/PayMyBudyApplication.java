0ccc51a3c3b2966cd6aba846474cfad108f2a29c
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-03-30 11:31:29 +0200

    Structure de l'appli

---
+++ PayMyBudy/src/main/java/com/pay/my/budy/PayMyBudyApplication.java
@@ -0,0 +1,13 @@
+package com.pay.my.budy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayMyBudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayMyBudyApplication.class, args);
+	}
+
+}
---
+++ PayMyBudy/src/test/java/com/pay/my/budy/PayMyBudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.my.budy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMyBudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
