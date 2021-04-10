09353d4020e5338e27e2d6ff3fdc3a39ed32384b
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-04-10 16:02:59 +0200

    ajout des dependances hibernate dans le pom.xml

--- PayMyBudy/src/main/java/com/pay/my/budy/PayMyBudyApplication.java
+++ PayMyBudy/src/main/java/com/pay/my/budy/PayMyBudyApplication.java
@@ -2,12 +2,18 @@ package com.pay.my.budy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication()
+import com.pay.my.budy.config.HibernateFactory;
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class PayMyBudyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayMyBudyApplication.class, args);
+		
+		HibernateFactory factory = new HibernateFactory();
+		factory.userFactory();
 	}
 
 }
