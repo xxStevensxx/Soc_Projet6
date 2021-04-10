082d5805f264dd59df0cd8f5c5c48cfa528a45ab
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2021-04-10 15:49:23 +0200

    ajout des packages Dao et Dao test avec leurs classes respectives

---
+++ PayMyBudy/src/test/java/com/pay/my/budy/dao/UserDaoTest.java
@@ -0,0 +1,76 @@
+package com.pay.my.budy.dao;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.pay.my.budy.model.User;
+
+public class UserDaoTest  {
+	
+	@Autowired
+	UserDao userDao;
+	
+	@Autowired
+	User user;
+	
+
+	@Test
+	public void addTestInterface() {
+	
+	//GIVEN
+//	user.setFisrtName("stevens");
+//	user.setName("Tavares");
+//	userDao.add(user);
+//	
+	//WHEN
+	
+	//THEN
+//	assertEquals(expected, actual);
+	
+	}
+	
+	@Test
+	public void updateTestInterface() {
+	
+	//GIVEN
+	
+	//WHEN
+	
+	//THEN
+	
+	}
+	
+	@Test
+	public void deleteTestInterface() {
+	
+	//GIVEN
+	
+	//WHEN
+	
+	//THEN
+	
+	}
+	
+	@Test
+	public void getUserTestInterface() {
+	
+	//GIVEN
+	
+	//WHEN
+	
+	//THEN
+	
+	}
+	
+	@Test
+	public void getAllTestInterface() {
+	
+	//GIVEN
+	
+	//WHEN
+	
+	//THEN
+	
+	}
+
+}
